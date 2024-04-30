ca00bb16a411e6510ecd316792b3d39716b31743
saivenkatasiva <msaivenkatasiva@gmail.com>
2024-04-30 12:13:56 +0530

    first installation of packages

--- 05.mogodb.sh
+++ 05.mogodb.sh
@@ -32,7 +32,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "copied MongoDB repo"
 
-dnf install mongodb-org -y &>> $LOGFILE
+dnf install mongod-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
 
