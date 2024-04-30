302ed585cb961fa8f71c4ee1301ed48371bbfd30
saivenkatasiva <msaivenkatasiva@gmail.com>
2024-04-30 11:12:29 +0530

    first installation of packages

--- 04-installation-packages.sh
+++ 04-installation-packages.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started excecuting at $TIMESTAMP" &>> $LOGFILE
+echo "script started excecuting at $TIMESTAMP by " &>> $LOGFILE
 
 VALIDATE () {
     if [ $1 -ne 0 ]
---
+++ 05.mogodb.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%f-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+VALIDATE (){
+    if [ $1 -ne 0]
+    then
+        echo -e "$2 ... $R failed $N"
+    else
+        echo -e "$2 ... $G success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R please run the code with root user $N"
+else
+    echo -e "$G you are a root user $N"
+exit 1
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copied MongoDB repo"
+
+dnf install MongoDB -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "enabiling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "restarting MongoDB"
